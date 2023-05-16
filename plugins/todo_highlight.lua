6167f40832517ceb1da90b361ef9f587d88a1932
Milton <milton.depaula@featurespace.co.uk>
2023-05-16 12:44:43 +0100

    adding explicit lsp and new command for todo list

--- plugins/todo_highlight.lua
+++ plugins/todo_highlight.lua
@@ -3,6 +3,8 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
-    event = "User AstroFile"
+    event = "User AstroFile",
+    cmd = "TodoQuickFix",
+    keys = { "<leader>Tt", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
   }
 }
